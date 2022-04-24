cf800becb9fe31c81b5102bfd320709dfc81e587
Syed Monirul Alam <Rambling.ctg@gmail.com>
2022-04-25 03:27:40 +0600

    added pagination api

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000;
 require('dotenv').config();
 const app = express();
@@ -23,17 +23,41 @@ async function run() {
     const productsCollection = client.db('emaJohn').collection('product');
 
     app.get('/products', async (req, res) => {
+      // console.log(req.query);
+      const page = parseInt(req.query.page);
+      const size = parseInt(req.query.size);
       const query = {};
       const cursor = productsCollection.find(query);
-      const products = await cursor.toArray();
+      let products;
+      if (page || size) {
+        // 0 --> skip: 0 get: 0-10(10):
+        // 1 --> skip: 1*10 get: 11-20(10):
+        // 2 --> skip: 2*10 get: 21-30 (10):
+        // 3 --> skip: 3*10 get: 21-30 (10):
+        products = await cursor
+          .skip(page * size)
+          .limit(size)
+          .toArray();
+      } else {
+        products = await cursor.toArray();
+      }
       res.send(products);
     });
 
     app.get('/productsCount', async (req, res) => {
-      // const query = {};
-      // const cursor = productsCollection.find(query);
       const count = await productsCollection.estimatedDocumentCount();
       res.send({ count }); // evabe korle object akare pavo value
+    });
+
+    // Use POST to get products by ids
+    app.post('/productsByKeys', async (req, res) => {
+      const keys = req.body;
+      const ids = keys.map((id) => ObjectId(id));
+      // console.log(keys);
+      const query = { _id: { $in: ids } };
+      const cursor = productsCollection.find(query);
+      const products = await cursor.toArray();
+      res.send(products);
     });
   } finally {
     console.log('Finally');
